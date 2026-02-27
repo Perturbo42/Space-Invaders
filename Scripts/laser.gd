276b9ad660302f5626526df120a1ace502d68c02
Perturbo42 <calvin.kim6@gmail.com>
2026-02-27 17:19:28 +0900

    Can now shoot lasers
    lasers also destroy themselves if off screen

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -1,1 +1,14 @@
 class_name Laser extends Area2D
+signal destroyed
+@export var speed : float
+
+func _ready() -> void:
+	speed = 300
+
+func _process(delta: float) -> void:
+	if global_position.y <= 0:
+		destroyed.emit()
+		queue_free()
+	else:
+		global_position.y -= speed * delta
+	
--- Scripts/spaceship.gd
+++ Scripts/spaceship.gd
@@ -1,11 +1,27 @@
 class_name Spaceship extends Area2D
 @export var speed : float
+var fired : bool
+const LASER = preload("res://Scenes/laser.tscn")
 
 func _ready() -> void:
 	speed = 150.0
+	fired = false
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left") and global_position.x >= 14:
 		position.x -= speed*delta
 	elif Input.is_action_pressed("right") and global_position.x <= 600:
 		position.x += speed*delta
+	
+	if Input.is_action_just_pressed("shoot") and !fired:
+		shoot()
+
+func shoot():
+	var laser = LASER.instantiate()
+	fired = true
+	laser.destroyed.connect(laser_destroyed)
+	laser.global_position = global_position
+	get_tree().current_scene.add_child(laser)
+
+func laser_destroyed():
+	fired = false
