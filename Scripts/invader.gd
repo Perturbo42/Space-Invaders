af3a5b8729ff80fcd0455343c8d80e373e69c6a3
Perturbo42 <calvin.kim6@gmail.com>
2026-03-03 17:25:15 +0900

    Added animations to invaders

--- Scripts/invader.gd
+++ Scripts/invader.gd
@@ -1,4 +1,19 @@
 class_name Invader extends Area2D
+@onready var sprite = $Sprite2D
+@export var timer : float
+@export var time : float
+
+func _ready() -> void:
+	timer = 0
+
+func _process(delta: float) -> void:
+	timer += delta
+	if timer >= time:
+		timer = 0
+		if sprite.frame == 0 : sprite.frame = 1
+		else : sprite.frame = 0
+	pass
+	
 
 func destroy_self():
 	queue_free()
--- Scripts/swarm.gd
+++ Scripts/swarm.gd
@@ -1,8 +1,15 @@
 class_name Swarm extends Node2D
-
-
+const INVADER = preload("res://Scenes/invader.tscn")
+@export var row_count : int
+@export var col_count : int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for i in range(row_count):
+		for j in range(col_count):
+			var alien = INVADER.instantiate()
+			alien.position.x = j * 40
+			alien.position.y = i * 50
+			add_child(alien)
 	pass # Replace with function body.
 
 
