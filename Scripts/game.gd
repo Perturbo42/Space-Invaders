67640e8ac0840ae2fec7790d6482dab684816d30
Perturbo42 <calvin.kim6@gmail.com>
2026-03-09 17:23:20 +0900

    Felt like adding scores
    gonna work on it more

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,9 +1,13 @@
 class_name Game extends Node2D
+@export var score : int = 0
 @onready var spaceship: Spaceship = $Spaceship
+
 @onready var gameover: Control = $GameOver
+@onready var score_text : Label = $GameOver/Score
 func _ready() -> void:
 	spaceship.dead.connect(game_over)
 	gameover.visible = false
+	score_text.text = str(score)
 
 func game_over():
 	gameover.visible = true
@@ -11,3 +15,6 @@ func game_over():
 func _on_button_pressed() -> void:
 	get_tree().reload_current_scene()
 	pass # Replace with function body.
+
+func add_score(points: int):
+	score += points
